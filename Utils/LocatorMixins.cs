02263d0aca29db026620f010e169be64e5f48b00
Lior Mualem <lior+git@ghb.xperitos.com>
2015-03-08 15:17:12 +0200

    Added more utils.

---
+++ Utils/LocatorMixins.cs
@@ -0,0 +1,37 @@
+﻿using Splat;
+
+namespace Xperitos.Common.Utils
+{
+    public static class LocatorMixins
+    {
+        /// <summary>
+        /// Registers a singleton.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="key">The key.</param>
+        /// <returns>The container.</returns>
+        public static IMutableDependencyResolver Singleton<TService, TImplementation>(this IMutableDependencyResolver container, string key = null)
+            where TImplementation : TService, new()
+        {
+            container.RegisterLazySingleton(() => new TImplementation(), typeof(TService), key);
+            return container;
+        }
+
+        /// <summary>
+        /// Registers an service to be created on each request.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementation.</typeparam>
+        /// <param name="container">The container.</param>
+        /// <param name="key">The key.</param>
+        /// <returns>The container.</returns>
+        public static IMutableDependencyResolver PerRequest<TService, TImplementation>(this IMutableDependencyResolver container, string key = null)
+            where TImplementation : TService, new()
+        {
+            container.Register(() => new TImplementation(), typeof(TService), key);
+            return container;
+        }
+    }
+}
---
+++ Utils/ObservableMixins.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace Xperitos.Common.Utils
+{
+    public static class ObservableMixins
+    {
+        /// <summary>
+        /// Convert to a unified return value.
+        /// </summary>
+        /// <param name="generateInitialValue">When true, an initial value will be merged with the returned observable</param>
+        /// <returns>A unit observable</returns>
+        public static IObservable<Unit> AsUnit<T>(this IObservable<T> obj, bool generateInitialValue = false)
+        {
+            if (generateInitialValue)
+                return obj
+                    .Select(v => Unit.Default)
+                    .Merge(Observable.Return(Unit.Default));
+
+            return obj.Select(v => Unit.Default);
+        }
+    }
+}
