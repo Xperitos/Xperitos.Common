eda5d4e1855a89cb497839f29a9b4d9816ef6493
Eran Bida <eran+git@ghb.xperitos.com>
2015-03-19 17:06:23 +0200

    Added Xperitos.Common.Web

---
+++ Utils/StreamMixins.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xperitos.Common.Utils
+{
+    public static class StreamMixins
+    {
+        public static async Task<string> ReadAllAsync(this Stream stream, Encoding encoding)
+        {
+            using (var mem = new System.IO.MemoryStream())
+            {
+                await stream.CopyToAsync(mem);
+                var bytes = mem.ToArray();
+                return encoding.GetString(bytes);
+            }
+        }
+        public static Task<string> ReadAllAsync(this Stream stream)
+        {
+            return ReadAllAsync(stream, Encoding.UTF8);
+        }
+    }
+}
