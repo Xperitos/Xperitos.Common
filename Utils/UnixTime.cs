0d478f4de8ef188210a803b8577c6b474999631c
Lior Mualem <lior+git@ghb.xperitos.com>
2015-04-29 16:16:28 +0300

    Added from unix time,.

---
+++ Utils/UnixTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Xperitos.Common.Utils
+{
+    public static class UnixTime
+    {
+        /// <summary>
+        /// Convert the given date time offset to unit time in milliseconds.
+        /// </summary>
+        public static long ToUnixTimeMS(this DateTimeOffset time)
+        {
+            return (long)(time - Epoch).TotalMilliseconds;
+        }
+
+        public static DateTimeOffset FromUnixTimeMS(long time)
+        {
+            return Epoch.AddMilliseconds(time);
+        }
+
+        private static readonly DateTimeOffset Epoch = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
+    }
+}
