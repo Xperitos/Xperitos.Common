9abeaad9611a9203412144d4b49fc1dc7a261fd9
Lior Mualem <lior+git@ghb.xperitos.com>
2015-07-01 16:03:15 +0300

    Added buffering using cb function.

---
+++ Utils/ObservableBufferMixins.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xperitos.Common.Utils
+{
+    static public class ObservableBufferMixins
+    {
+        /// <summary>
+        /// Overload the buffer function that accepts a closing selector - it receives the current buffer and return true if it should close.
+        /// </summary>
+        static public IObservable<IList<T>> Buffer<T>(this IObservable<T> observable, Func<IList<T>, bool> closingSelector)
+        {
+            var refCounted = observable.Publish().RefCount();
+
+            var currentBuffer = new List<T>();
+
+            return Observable.Create<IList<T>>(
+                observer =>
+                {
+                    return refCounted.Subscribe((v) =>
+                    {
+                        currentBuffer.Add(v);
+                        if (closingSelector(currentBuffer))
+                        {
+                            var buffer = currentBuffer;
+                            currentBuffer = new List<T>();
+
+                            // Pass it to the observer.
+                            observer.OnNext(buffer);
+                        }
+                    }, observer.OnError, observer.OnCompleted);
+                });
+        }
+    }
+}
