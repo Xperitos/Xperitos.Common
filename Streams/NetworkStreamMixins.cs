1b47a363145e2d1e0525f5de50c2ee087d1a29ab
Lior Mualem <lior+git@ghb.xperitos.com>
2015-07-05 17:04:33 +0300

    Added cancellable network stream reader.

---
+++ Streams/NetworkStreamMixins.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Xperitos.Common.Streams
+{
+    public static class NetworkStreamMixins
+    {
+        /// <summary>
+        /// Implements a read async that supports cancellation token properly (using timeouts).
+        /// </summary>
+        public static Task<int> CancellableReadAsync(this NetworkStream stream, byte[] buffer, int offset, int count, CancellationToken ct)
+        {
+            return Task.Run(() =>
+            {
+                while (true)
+                {
+                    ct.ThrowIfCancellationRequested();
+                    try
+                    {
+                        stream.ReadTimeout = 1000;
+                        return stream.Read(buffer, offset, count);
+                    }
+                    catch (IOException e)
+                    {
+                        var socketEx = e.InnerException as SocketException;
+
+                        // If timedout - then just repeat the loop.
+                        if (socketEx != null && socketEx.SocketErrorCode == SocketError.TimedOut)
+                            continue;
+
+                        throw;
+                    }
+                }
+            }, ct);
+        }
+
+    }
+}
