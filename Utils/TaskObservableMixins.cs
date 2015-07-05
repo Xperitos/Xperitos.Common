7dae739d7a3053703f6477e9a54e00b142cae18c
Lior Mualem <lior+git@ghb.xperitos.com>
2015-07-05 14:04:00 +0300

    Added Unwrap mixin.

---
+++ Utils/TaskObservableMixins.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
+
+namespace Xperitos.Common.Utils
+{
+    public static class TaskObservableMixins
+    {
+        /// <summary>
+        /// Extract the result of a task as a stream of results.
+        /// </summary>
+        public static IObservable<T> Unwrap<T>(this IObservable<Task<T>> observable)
+        {
+            return observable.SelectMany(v => TaskObservableExtensions.ToObservable<T>(v));
+        }
+    }
+}
