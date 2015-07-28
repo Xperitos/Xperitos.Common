e8d8061092a2d2b05ccfbd87869610a7c22f7f6d
Lior Mualem <lior+git@ghb.xperitos.com>
2015-07-28 11:16:19 +0300

    Added convinence function.

--- Utils/SubscribeAsyncMixins.cs
+++ Utils/SubscribeAsyncMixins.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive;
 using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
 using System.Threading;
@@ -10,6 +11,19 @@ namespace Xperitos.Common.Utils
     public static class SubscribeAsyncMixins
     {
         /// <summary>
+        /// Serialize the calls to the action callback - unit overload for simpler functions.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="observable">Sequence of events</param>
+        /// <param name="action">Async action to run for each event.</param>
+        /// <param name="scheduler">Where the action execution takes place</param>
+        /// <param name="maxPendingEvents">If queued events exceeds this much items, and overflow exception is thrown</param>
+        public static IDisposable SubscribeAsync(this IObservable<Unit> observable, Func<CancellationToken, Task> action, IScheduler scheduler, int maxPendingEvents = 100)
+        {
+            return observable.SubscribeAsync((v, ct) => action(ct), scheduler, maxPendingEvents);
+        }
+
+        /// <summary>
         /// Serialize the calls to the action callback
         /// </summary>
         /// <typeparam name="T"></typeparam>
@@ -17,8 +31,7 @@ namespace Xperitos.Common.Utils
         /// <param name="action">Async action to run for each event.</param>
         /// <param name="scheduler">Where the action execution takes place</param>
         /// <param name="maxPendingEvents">If queued events exceeds this much items, and overflow exception is thrown</param>
-        /// <returns></returns>
-        public static IDisposable SubscribeAsync<T>(this IObservable<T> observable, Func<T, CancellationToken, Task> action, IScheduler scheduler = null, int maxPendingEvents = 100)
+        public static IDisposable SubscribeAsync<T>(this IObservable<T> observable, Func<T, CancellationToken, Task> action, IScheduler scheduler, int maxPendingEvents = 100)
         {
             if (scheduler == null)
                 scheduler = Scheduler.Default;
