ca63f230287b362631befc80a638b1a694c3c0e2
Lior Mualem <lior+git@ghb.xperitos.com>
2015-08-09 11:02:54 +0300

    Added "name for" method - to obtain property name using reflectino.

---
+++ Utils/NameFor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Xperitos.Common.Utils
+{
+    /// <summary>
+    /// Convinence class to obtain a contant property name
+    /// </summary>
+    public static class NameFor<T>
+    {
+        public static string Property<TRet>(Expression<Func<T, TRet>> property)
+        {
+            return RxObjectMixins.PropertyName(property);
+        }
+    }
+}
