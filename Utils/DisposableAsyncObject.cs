87419fc144b2757f9fd1c80fd5a90c3717ec73b1
Lior Mualem <lior+git@ghb.xperitos.com>
2015-10-20 14:17:07 +0300

    Implemented ReactiveUI-less DisposableAsyncObject.

---
+++ Utils/DisposableAsyncObject.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Xperitos.Common.Utils
+{
+    public class DisposableAsyncObject : INotifyPropertyChanged, INotifyPropertyChanging, IDisposable
+    {
+        protected DisposableAsyncObject(IScheduler scheduler)
+        {
+            Scheduler = scheduler;
+            Disposables = new CompositeDisposable();
+
+            m_tokenSource = new CancellationDisposable();
+            DisposedToken = m_tokenSource.Token;
+        }
+
+        /// <summary>
+        /// The scheduler associated with this async object.
+        /// </summary>
+        public IScheduler Scheduler { get; private set; }
+
+        /// <summary>
+        /// Use this in derived classes to register disposable objects.
+        /// </summary>
+        protected CompositeDisposable Disposables { get; private set; }
+
+        /// <summary>
+        /// Watch this for service disposal event.
+        /// </summary>
+        protected CancellationToken DisposedToken { get; private set; }
+
+        private readonly CancellationDisposable m_tokenSource;
+
+        public void Dispose()
+        {
+            if (Disposables.IsDisposed)
+                return;
+
+            m_tokenSource.Dispose();
+
+            Disposables.Dispose();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        internal void RaisePropertyChanging(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        internal void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
+    }
+}
---
+++ Utils/DisposableAsyncObjectMixins.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Xperitos.Common.Utils
+{
+    /// <summary>
+    /// Helper functions for DisposableAsyncObject.
+    /// </summary>
+    /// <remarks>Some functions were taken from IReactiveObject.cs (of ReactiveUI)</remarks>
+    public static class DisposableAsyncObjectMixins
+    {
+        /// <summary>
+        /// RaiseAndSetIfChanged fully implements a Setter for a read-write
+        /// property on a DisposableAsyncObject, using CallerMemberName to raise the notification
+        /// and the ref to the backing field to set the property.
+        /// </summary>
+        /// <typeparam name="TObj">The type of the This.</typeparam>
+        /// <typeparam name="TRet">The type of the return value.</typeparam>
+        /// <param name="This"></param>
+        /// <param name="backingField">A Reference to the backing field for this
+        /// property.</param>
+        /// <param name="newValue">The new value.</param>
+        /// <param name="propertyName">The name of the property, usually 
+        /// automatically provided through the CallerMemberName attribute.</param>
+        /// <returns>The newly set value, normally discarded.</returns>
+        public static TRet RaiseAndSetIfChanged<TObj, TRet>(
+            this TObj This,
+            ref TRet backingField,
+            TRet newValue,
+            [CallerMemberName] string propertyName = null) where TObj : DisposableAsyncObject
+        {
+            if (EqualityComparer<TRet>.Default.Equals(backingField, newValue))
+                return newValue;
+
+            This.RaisePropertyChanging(propertyName);
+            backingField = newValue;
+            This.RaisePropertyChanged(propertyName);
+            return newValue;
+        }
+    }
+}
