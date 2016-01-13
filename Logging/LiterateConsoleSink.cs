fc31c3cd5e170710ccee828dfa05f65bd44878f9
Lior Mualem <lior+git@ghb.xperitos.com>
2016-01-13 19:41:44 +0200

    * Added functions to run process async and obtain the results. * Removed usage of SPLAT! - Serilog is the default logger. * Added a nice LiterateConsoleSink (derived from the original sink with changes).

---
+++ Logging/LiterateConsoleSink.cs
@@ -0,0 +1,312 @@
+﻿// Copyright 2015 Serilog Contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+using Serilog.Parsing;
+
+namespace Serilog.Sinks.Literate
+{    
+    public class LiterateConsoleSink : ILogEventSink
+    {
+        const ConsoleColor Text = ConsoleColor.White,
+                           Subtext = ConsoleColor.Gray,
+                           Punctuation = ConsoleColor.DarkGray,
+
+                           VerboseLevel = ConsoleColor.Gray,
+                           DebugLevel = VerboseLevel,
+                           InformationLevel = ConsoleColor.White,
+                           WarningLevel = ConsoleColor.Yellow,
+                           ErrorLevel = ConsoleColor.Red,
+                           FatalLevel = ErrorLevel,
+
+                           KeywordSymbol = ConsoleColor.Blue,
+                           NumericSymbol = ConsoleColor.Magenta,
+                           StringSymbol = ConsoleColor.Cyan,
+                           OtherSymbol = ConsoleColor.Green,
+                           NameSymbol = Subtext,
+                           RawText = ConsoleColor.Yellow;
+
+        const string StackFrameLinePrefix = "   ";
+
+        class LevelFormat
+        {
+            readonly string _description;
+            readonly ConsoleColor _color;
+
+            public LevelFormat(string description, ConsoleColor color)
+            {
+                _description = description;
+                _color = color;
+            }
+
+            public string Description { get { return _description; } }
+            public ConsoleColor Color { get { return _color; } }
+        }
+
+        readonly IDictionary<LogEventLevel, LevelFormat> _levels = new Dictionary<LogEventLevel, LevelFormat>
+        {
+            { LogEventLevel.Verbose, new LevelFormat("VRB", VerboseLevel) },
+            { LogEventLevel.Debug, new LevelFormat("DBG", DebugLevel) },
+            { LogEventLevel.Information, new LevelFormat("INF", InformationLevel) },
+            { LogEventLevel.Warning, new LevelFormat("WRN", WarningLevel) },
+            { LogEventLevel.Error, new LevelFormat("ERR", ErrorLevel) },
+            { LogEventLevel.Fatal, new LevelFormat("FTL", FatalLevel) },
+        };
+
+        readonly IFormatProvider _formatProvider;
+        readonly object _syncRoot = new object();
+        readonly MessageTemplate _outputTemplate;
+
+        public LiterateConsoleSink(string outputTemplate, IFormatProvider formatProvider)
+        {
+            if (outputTemplate == null) throw new ArgumentNullException("outputTemplate");
+            _outputTemplate = new MessageTemplateParser().Parse(outputTemplate);
+            _formatProvider = formatProvider;
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            if (logEvent == null) throw new ArgumentNullException("logEvent");
+
+            var outputProperties = OutputProperties.GetOutputProperties(logEvent);
+            
+            lock (_syncRoot)
+            {
+                try
+                {
+                    foreach (var outputToken in _outputTemplate.Tokens)
+                    {
+                        var propertyToken = outputToken as PropertyToken;
+                        if (propertyToken == null)
+                        {
+                            RenderOutputTemplateTextToken(outputToken, outputProperties);
+                        }
+                        else switch (propertyToken.PropertyName)
+                        {
+                            case OutputProperties.LevelPropertyName:
+                                RenderLevelToken(logEvent.Level);
+                                break;
+                            case OutputProperties.MessagePropertyName:
+                                RenderMessageToken(logEvent);
+                                break;
+                            case OutputProperties.ExceptionPropertyName:
+                                RenderExceptionToken(propertyToken, outputProperties);
+                                break;
+                            default:
+                                if (outputProperties.ContainsKey(propertyToken.PropertyName))
+                                    RenderOutputTemplatePropertyToken(outputToken, outputProperties);
+                                break;
+                        }
+                    }
+                }
+                finally { Console.ResetColor(); }
+            }
+        }
+
+        void RenderExceptionToken(MessageTemplateToken outputToken, IReadOnlyDictionary<string, LogEventPropertyValue> outputProperties)
+        {
+            var sw = new StringWriter();
+            outputToken.Render(outputProperties, sw, _formatProvider);
+            var lines = new StringReader(sw.ToString());
+            string nextLine;
+            while ((nextLine = lines.ReadLine()) != null)
+            {
+                Console.ForegroundColor = nextLine.StartsWith(StackFrameLinePrefix) ? Subtext : Text;
+                Console.WriteLine(nextLine);
+            }
+        }
+
+        void RenderOutputTemplatePropertyToken(MessageTemplateToken outputToken, IReadOnlyDictionary<string, LogEventPropertyValue> outputProperties)
+        {
+            Console.ForegroundColor = Subtext;
+            outputToken.Render(outputProperties, Console.Out, _formatProvider);
+        }
+
+        void RenderLevelToken(LogEventLevel level)
+        {
+            LevelFormat format;
+            if (!_levels.TryGetValue(level, out format))
+                format = _levels[LogEventLevel.Warning];
+
+            Console.ForegroundColor = format.Color;
+
+            if (level == LogEventLevel.Error || level == LogEventLevel.Fatal)
+            {
+                Console.BackgroundColor = format.Color;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.Write(format.Description);
+            Console.ResetColor();
+        }
+
+        void RenderOutputTemplateTextToken(MessageTemplateToken outputToken, IReadOnlyDictionary<string, LogEventPropertyValue> outputProperties)
+        {
+            Console.ForegroundColor = Punctuation;
+            outputToken.Render(outputProperties, Console.Out, _formatProvider);
+        }
+
+        void RenderMessageToken(LogEvent logEvent)
+        {
+            foreach (var messageToken in logEvent.MessageTemplate.Tokens)
+            {
+                var messagePropertyToken = messageToken as PropertyToken;
+                if (messagePropertyToken != null)
+                {
+                    LogEventPropertyValue value;
+                    if (!logEvent.Properties.TryGetValue(messagePropertyToken.PropertyName, out value))
+                    {
+                        Console.ForegroundColor = RawText;
+                        Console.Write(messagePropertyToken);
+                    }
+                    else
+                    {
+                        var scalar = value as ScalarValue;
+                        if (scalar != null)
+                        {
+                            Console.ForegroundColor = GetScalarColor(scalar);
+
+                            if (scalar.Value is string && messagePropertyToken.Format == null && messagePropertyToken.Alignment == null)
+                                Console.Write(scalar.Value);
+                            else
+                                messagePropertyToken.Render(logEvent.Properties, Console.Out, _formatProvider);
+                        }
+                        else
+                        {
+                            PrettyPrint(value, messagePropertyToken.Format, _formatProvider);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = Text;
+                    messageToken.Render(logEvent.Properties, Console.Out, _formatProvider);
+                }
+            }
+        }
+
+        void PrettyPrint(LogEventPropertyValue value, string format, IFormatProvider formatProvider)
+        {
+            var scalar = value as ScalarValue;
+            if (scalar != null)
+            {
+                Console.ForegroundColor = GetScalarColor(scalar);
+                value.Render(Console.Out, format, formatProvider);
+                return;
+            }
+
+            var seq = value as SequenceValue;
+            if (seq != null)
+            {
+                Console.ForegroundColor = Punctuation;
+                Console.Write("[");
+
+                var sep = "";
+                foreach (var element in seq.Elements)
+                {
+                    Console.ForegroundColor = Punctuation;
+                    Console.Write(sep);
+                    sep = ", ";
+
+                    PrettyPrint(element, null, formatProvider);
+                }
+
+                Console.ForegroundColor = Punctuation;
+                Console.Write("]");
+                return;
+            }
+
+            var str = value as StructureValue;
+            if (str != null)
+            {
+                if (str.TypeTag != null)
+                {
+                    Console.ForegroundColor = Subtext;
+                    Console.Write(str.TypeTag);
+                    Console.Write(" ");
+                }
+
+                Console.ForegroundColor = Punctuation;
+                Console.Write("{");
+
+                var sep = "";
+                foreach (var prop in str.Properties)
+                {
+                    Console.ForegroundColor = Punctuation;
+                    Console.Write(sep);
+                    sep = ", ";
+
+                    Console.ForegroundColor = NameSymbol;
+                    Console.Write(prop.Name);
+
+                    Console.ForegroundColor = Punctuation;
+                    Console.Write("=");
+
+                    PrettyPrint(prop.Value, null, formatProvider);
+                }
+
+                Console.ForegroundColor = Punctuation;
+                Console.Write("}");
+                return;
+            }
+
+            var div = value as DictionaryValue;
+            if (div != null)
+            {
+                Console.ForegroundColor = Punctuation;
+                Console.Write("{");
+
+                var sep = "";
+                foreach (var element in div.Elements)
+                {
+                    Console.ForegroundColor = Punctuation;
+                    Console.Write(sep);
+                    sep = ", ";
+                    Console.Write("[");
+                    PrettyPrint(element.Key, null, formatProvider);
+
+                    Console.ForegroundColor = Punctuation;
+                    Console.Write("]=");
+
+                    PrettyPrint(element.Value, null, formatProvider);
+                }
+
+                Console.ForegroundColor = Punctuation;
+                Console.Write("}");
+                return;
+            }
+
+            value.Render(Console.Out, format, formatProvider);
+        }
+
+        ConsoleColor GetScalarColor(ScalarValue scalar)
+        {
+            if (scalar.Value == null || scalar.Value is bool)
+                return KeywordSymbol;
+            
+            if (scalar.Value is string)
+                return StringSymbol;
+            
+            if (scalar.Value.GetType().IsPrimitive || scalar.Value is decimal)
+                return NumericSymbol;
+            
+            return OtherSymbol;
+        }
+    }
+}
---
+++ Utils/ProcessHelper.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace Xperitos.Common.Utils
+{
+    public static class ProcessHelper
+    {
+        /// <summary>
+        /// Creates a cold observable that runs the given command and returns its stdout
+        /// </summary>
+        /// <param name="fileName">command to run</param>
+        /// <param name="args">arguments for the command</param>
+        /// <returns>Connectable observable - runs the command upon connection and returns each output line from the command</returns>
+        public static IConnectableObservable<string> ObserveCommand(string fileName, string args = null)
+        {
+            return Observable.Create<string>(
+                (observer, ct) =>
+                {
+                    Log.Logger.Debug("Running command {exeFile} with {args}", fileName, args);
+
+                    var process = new Process
+                    {
+                        StartInfo =
+                        {
+                            UseShellExecute = false,
+                            FileName = fileName,
+                            Arguments = args,
+                            WindowStyle = ProcessWindowStyle.Hidden,
+
+                            // Redirect the output stream of the child process.
+                            RedirectStandardOutput = true,
+                            RedirectStandardError = true
+                        }
+                    };
+
+                    var registration = ct.Register(() =>
+                    {
+                        if (process != null && !process.HasExited)
+                            process.Kill();
+                    });
+
+                    return Task.Factory.StartNew(() =>
+                    {
+                        process.Start();
+                        bool hadError = false;
+
+                        // Do not wait for the child process to exit before
+                        // reading to the end of its redirected stream.
+                        // p.WaitForExit();
+                        // Read the output stream first and then wait.
+                        try
+                        {
+                            while (!process.StandardOutput.EndOfStream)
+                                observer.OnNext(process.StandardOutput.ReadLine());
+                            process.WaitForExit();
+                        }
+                        catch (Exception e)
+                        {
+                            observer.OnError(e);
+                            hadError = true;
+                        }
+
+                        Log.Logger.Debug("Command {exeFile} with {args} terminated", fileName, args);
+
+                        registration.Dispose();
+                        process.Dispose();
+                        process = null;
+
+                        if (!hadError)
+                            observer.OnCompleted();
+
+                    }, ct, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+                }).Publish();
+        }
+
+        /// <summary>
+        /// Run process and return the exit code (or null if cancelled before started running).
+        /// </summary>
+        /// <param name="fileName">Process to run</param>
+        /// <param name="args">Arguments for the process</param>
+        /// <param name="ct">Cancellation token. When cancelled, process will be killed!</param>
+        /// <returns>Task with process exit code</returns>
+        public static Task<int?> RunProcessAsync(string fileName, string args = null, CancellationToken ct = default(CancellationToken))
+        {
+            if (ct.IsCancellationRequested)
+                return Task.FromResult<int?>(null);
+
+            var tcs = new TaskCompletionSource<int?>();
+
+            Log.Logger.Debug("Running command {exeFile} with {args}", fileName, args);
+
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    UseShellExecute = false,
+                    FileName = fileName,
+                    Arguments = args,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                },
+                EnableRaisingEvents = true,
+            };
+
+            var registration = ct.Register(process.Kill);
+
+            process.Exited += (sender, e) =>
+            {
+                Log.Logger.Debug("Command {exeFile} with {args} terminated", fileName, args);
+                tcs.SetResult(process.ExitCode);
+                registration.Dispose();
+                process.Dispose();
+            };
+
+            process.Start();
+
+            return tcs.Task;
+        }
+    }
+}
