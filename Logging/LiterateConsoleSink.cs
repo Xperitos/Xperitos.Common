f86a767de712c9b392001efd7d427253144685f0
Lior Mualem <lior+git@ghb.xperitos.com>
2016-02-03 11:25:00 +0200

    Added missing class.

--- Logging/LiterateConsoleSink.cs
+++ Logging/LiterateConsoleSink.cs
@@ -345,4 +345,45 @@ namespace Serilog.Sinks.Literate
             return OtherSymbol;
         }
     }
+
+    // A special case (non-null) string value for use in output
+    // templates. Does not apply "quoted" formatting by default.
+    class LiteralStringValue : LogEventPropertyValue
+    {
+        readonly string _value;
+
+        public LiteralStringValue(string value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+            _value = value;
+        }
+
+        public override void Render(TextWriter output, string format = null, IFormatProvider formatProvider = null)
+        {
+            var toRender = _value;
+
+            switch (format)
+            {
+                case "u":
+                    toRender = _value.ToUpperInvariant();
+                    break;
+                case "w":
+                    toRender = _value.ToLowerInvariant();
+                    break;
+            }
+
+            output.Write(toRender);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var sv = obj as LiteralStringValue;
+            return sv != null && Equals(_value, sv._value);
+        }
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+    }
 }
