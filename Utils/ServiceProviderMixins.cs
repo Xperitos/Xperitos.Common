e92e5140382931cc28c165ddc3f7dd6cb3155ef9
Lior Mualem <lior+git@ghb.xperitos.com>
2016-02-04 07:56:04 +0200

    Added IServiceProvider support class.

---
+++ Utils/ServiceProviderMixins.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xperitos.Common.Utils
+{
+    public static class ServiceProviderMixins
+    {
+        /// <summary>
+        /// Type-safe overload for get service.
+        /// </summary>
+        public static T GetService<T>(this IServiceProvider provider)
+        {
+            return (T)provider.GetService(typeof(T));
+        }
+    }
+}
