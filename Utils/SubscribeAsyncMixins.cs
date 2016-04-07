e8e500289c747923ce66bb211341ad5414f76b28
Lior Mualem <lior+git@ghb.xperitos.com>
2016-04-07 10:05:55 +0300

    Improved logic for "unit" SubscribeAsync and renamed method to SubscribeSingleAsync. It wont enqueue the calls since the general usage is simply to schedule the action to run. Instead, it will just schedule the action to run.

--- Utils/SubscribeAsyncMixins.cs
+++ Utils/SubscribeAsyncMixins.cs
@@ -11,16 +11,57 @@ namespace Xperitos.Common.Utils
     public static class SubscribeAsyncMixins
     {
         /// <summary>
-        /// Serialize the calls to the action callback - unit overload for simpler functions.
+        /// Serialize the calls to the action callback - the action call will be re-scheduled only once if multiple events are received while it was still running.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
         /// <param name="observable">Sequence of events</param>
         /// <param name="action">Async action to run for each event.</param>
         /// <param name="scheduler">Where the action execution takes place</param>
-        /// <param name="maxPendingEvents">If queued events exceeds this much items, and overflow exception is thrown</param>
-        public static IDisposable SubscribeAsync(this IObservable<Unit> observable, Func<CancellationToken, Task> action, IScheduler scheduler, int maxPendingEvents = 100)
+        public static IDisposable SubscribeSingleAsync(this IObservable<Unit> observable, Func<CancellationToken, Task> action, IScheduler scheduler)
         {
-            return observable.SubscribeAsync((v, ct) => action(ct), scheduler, maxPendingEvents);
+            if (scheduler == null)
+                scheduler = Scheduler.Default;
+
+            // Queue of items + sync object.
+            var runActionEvent = new ManualResetEventSlim(false);
+            var disposables = new CompositeDisposable();
+
+            // Create the pump loop.
+            scheduler.ScheduleAsync(async (innerScheduler, ct) =>
+            {
+                while (!ct.IsCancellationRequested)
+                {
+                    try
+                    {
+                        // Wait for a request to run the action.
+                        await Task.Run(() => runActionEvent.Wait(ct), ct);
+
+                        // Clear the event before the action is executed.
+                        runActionEvent.Reset();
+
+                        // Perform the action.
+                        await action(ct);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        // Catch cancelled exception (but only if it's ours).
+                        if (!ct.IsCancellationRequested)
+                            throw;
+                    }
+
+                    // Yield to the scheduler.
+                    await innerScheduler.Yield();
+                }
+            }).ComposeDispose(disposables);
+
+            // Subscribe to events.
+            observable
+                .Subscribe( 
+                    v => runActionEvent.Set(),
+                    e => disposables.Dispose(),
+                    () => disposables.Dispose())
+                .ComposeDispose(disposables);
+
+            return disposables;
         }
 
         /// <summary>
