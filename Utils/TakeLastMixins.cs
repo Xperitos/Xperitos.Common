2cdab96bfc636d134b8722656edd3ead173cccb5
Lior Mualem <lior+git@ghb.xperitos.com>
2016-06-13 16:23:05 +0300

    Added take last X elements

--- Utils/TakeLastMixins.cs
+++ Utils/TakeLastMixins.cs
@@ -6,6 +6,22 @@ namespace Xperitos.Common.Utils
     public static class TakeLastMixins
     {
         /// <summary>
+        /// Return all elements except the last X count
+        /// </summary>
+        public static IEnumerable<T> SkipLast<T>(this IEnumerable<T> list, int count)
+        {
+            // Try fast track first.
+            ICollection<T> collection = list as ICollection<T>;
+            if (collection == null)
+                collection = list.ToList();
+
+            if (collection.Count <= count)
+                return Enumerable.Empty<T>();
+
+            return collection.Take(collection.Count - count);
+        }
+
+        /// <summary>
         /// Returns the last <paramref name="count"/> elements.
         /// </summary>
         /// <remarks>Function doesn't return until enumeration completes</remarks>
