ff8692f6934e3bf1192b0d1c70d86877c462212c
Gal <gal.liberman+git@ghb.xperitos.com>
2016-08-25 14:32:27 +0300

    Add std calculation function

--- MathHelpers/MathUtils.cs
+++ MathHelpers/MathUtils.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Xperitos.Common.MathHelpers
 {
@@ -35,6 +37,26 @@ namespace Xperitos.Common.MathHelpers
         public static long LCM(long a, long b)
         {
             return a * b / GCD(a, b);
+        }
+        
+        /// <summary>
+        /// Calculates the std
+        /// </summary>
+        /// <param name="values">Values for calculation</param>
+        /// <returns></returns>
+        public static double CalculateStdDev(IEnumerable<double> values)
+        {
+            double ret = 0;
+            if (values.Count() > 0)
+            {
+                //Compute the Average      
+                double avg = values.Average();
+                //Perform the Sum of (value-avg)_2_2      
+                double sum = values.Sum(d => Math.Pow(d - avg, 2));
+                //Put it all together      
+                ret = Math.Sqrt((sum) / (values.Count() - 1));
+            }
+            return ret;
         }
     }
 }
