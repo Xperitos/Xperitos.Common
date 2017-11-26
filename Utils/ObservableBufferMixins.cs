e3855d03a1c0e96ef24fcd244f4e293a02692b8b
Lior Mualem <lior+git@ghb.xperitos.com>
2017-11-26 08:12:20 +0200

    Fixed buffer function - it didn't emit the last buffer.

--- Utils/ObservableBufferMixins.cs
+++ Utils/ObservableBufferMixins.cs
@@ -9,10 +9,40 @@ namespace Xperitos.Common.Utils
 {
     static public class ObservableBufferMixins
     {
-        /// <summary>
-        /// Overload the buffer function that accepts a closing selector - it receives the current buffer and return true if it should close.
-        /// </summary>
-        static public IObservable<IList<T>> Buffer<T>(this IObservable<T> observable, Func<IList<T>, bool> closingSelector)
+	    /// <summary>
+	    /// Overload the buffer function that accepts a closing selector - it receives the current buffer and return true if it should close.
+	    /// </summary>
+	    static public IObservable<IList<T>> Buffer<T>(this IObservable<T> observable, Func<IList<T>, T, bool> closingSelector)
+	    {
+		    return Observable.Create<IList<T>>(
+			    observer =>
+			    {
+				    var currentBuffer = new List<T>();
+
+				    return observable.Subscribe((v) =>
+				    {
+						if (currentBuffer.Count > 0 && closingSelector(currentBuffer, v))
+					    {
+						    var buffer = currentBuffer;
+						    currentBuffer = new List<T>();
+
+						    // Pass it to the observer.
+						    observer.OnNext(buffer);
+					    }
+					    currentBuffer.Add(v);
+				    }, observer.OnError, () =>
+				    {
+					    if (currentBuffer.Count > 0)
+						    observer.OnNext(currentBuffer);
+						observer.OnCompleted();
+				    });
+			    });
+	    }
+
+		/// <summary>
+		/// Overload the buffer function that accepts a closing selector - it receives the current buffer and return true if it should close.
+		/// </summary>
+		static public IObservable<IList<T>> Buffer<T>(this IObservable<T> observable, Func<IList<T>, bool> closingSelector)
         {
             return Observable.Create<IList<T>>(
                 observer =>
@@ -30,7 +60,12 @@ namespace Xperitos.Common.Utils
                             // Pass it to the observer.
                             observer.OnNext(buffer);
                         }
-                    }, observer.OnError, observer.OnCompleted);
+                    }, observer.OnError, () =>
+                    {
+						if (currentBuffer.Count > 0)
+							observer.OnNext(currentBuffer);
+	                    observer.OnCompleted();
+                    });
                 });
         }
     }
