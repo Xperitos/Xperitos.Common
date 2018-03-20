186988356e748bd2bb01701a29418a62e47beb23
Lior Mualem <lior+git@ghb.xperitos.com>
2018-03-20 09:06:34 +0200

    Added "forget" method for async functions.

---
+++ Utils/TasksMixins.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xperitos.Common.Utils
+{
+	public static class TasksMixins
+	{
+		/// <summary>
+		/// Use the task without doing anything with it. Useful for calling async tasks from sync code (and suppress the CS4014 warning).
+		/// </summary>
+		public static async void Forget(this Task task)
+		{
+			await task.ConfigureAwait(false);
+		}
+
+		/// <summary>
+		/// Use the task without doing anything with it. Useful for calling async tasks from sync code (and suppress the CS4014 warning).
+		/// </summary>
+		public static async void ForgetLogExceptions(this Task task, Action<Exception> exceptionLogger = null)
+		{
+			try
+			{
+				await task.ConfigureAwait(false);
+			}
+			catch (Exception e)
+			{
+				exceptionLogger?.Invoke(e);
+			}
+		}
+	}
+}
