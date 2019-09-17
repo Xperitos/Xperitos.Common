8d5f606176a5d8a4e3dd0e8a8b2a2f5dc607ce9d
Lior Mualem <lior+git@ghb.xperitos.com>
2019-09-17 08:43:05 +0300

    Added ability to specify logging formatter

--- Logging/RollingFileSinkMixins.cs
+++ Logging/RollingFileSinkMixins.cs
@@ -2,6 +2,7 @@ using System;
 using Serilog;
 using Serilog.Configuration;
 using Serilog.Events;
+using Serilog.Formatting;
 using Serilog.Formatting.Display;
 
 namespace Xperitos.Common.Logging
@@ -24,19 +25,33 @@ namespace Xperitos.Common.Logging
         }
 
         public static LoggerConfiguration XpRollingFileByDate(
-            this LoggerSinkConfiguration sinkConfiguration, 
-            string pathFormat, 
-            LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose, 
-            string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}", 
-            IFormatProvider formatProvider = null, 
-            long? fileSizeLimitBytes = 100*1024*1024,
-            int retainedDays = 60,
-            long? maxRetainedTotalFileSizeBytes = 2 * 1024 * 1024 * 1024L)
+	        this LoggerSinkConfiguration sinkConfiguration,
+	        string pathFormat,
+	        LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose,
+	        string outputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}",
+	        IFormatProvider formatProvider = null,
+	        long? fileSizeLimitBytes = 100 * 1024 * 1024,
+	        int retainedDays = 60,
+	        long? maxRetainedTotalFileSizeBytes = 2 * 1024 * 1024 * 1024L)
         {
-            MessageTemplateTextFormatter templateTextFormatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
-            var sink = new DateRollingFileSink(pathFormat, templateTextFormatter, fileSizeLimitBytes, retainedDays, maxRetainedTotalFileSizeBytes);
+	        MessageTemplateTextFormatter templateTextFormatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
+	        var sink = new DateRollingFileSink(pathFormat, templateTextFormatter, fileSizeLimitBytes, retainedDays, maxRetainedTotalFileSizeBytes);
 
-            return sinkConfiguration.Sink(sink, restrictedToMinimumLevel);
+	        return sinkConfiguration.Sink(sink, restrictedToMinimumLevel);
+        }
+
+        public static LoggerConfiguration XpRollingFileByDate(
+	        this LoggerSinkConfiguration sinkConfiguration,
+	        string pathFormat,
+	        ITextFormatter formatter,
+	        LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose,
+	        long? fileSizeLimitBytes = 100 * 1024 * 1024,
+	        int retainedDays = 60,
+	        long? maxRetainedTotalFileSizeBytes = 2 * 1024 * 1024 * 1024L)
+        {
+	        var sink = new DateRollingFileSink(pathFormat, formatter, fileSizeLimitBytes, retainedDays, maxRetainedTotalFileSizeBytes);
+
+	        return sinkConfiguration.Sink(sink, restrictedToMinimumLevel);
         }
     }
 }
